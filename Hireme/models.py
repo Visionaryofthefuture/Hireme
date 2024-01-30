4d248686b30ed42685b2c3551a617c7a828ea8ea
Visionaryofthefuture <somud2004@gmail.com>
2024-01-30 13:16:30 +0530

    file extension validated

--- Hireme/models.py
+++ Hireme/models.py
@@ -1,12 +1,19 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.core.validators import FileExtensionValidator
+
 # Create your models here.
 class User(models.Model):
     username = models.CharField(default = '', max_length = 255)
     email = models.EmailField(default = "", null = False, blank = False)
-    resume = models.FileField(default = None, upload_to="media")
+    resume = models.FileField(default = None, upload_to="media", validators= [FileExtensionValidator(["pdf"])])
+
+
+
+    def __str__(self):
+        return self.username
+
 
 
 class Employer(User):
     company_name = models.CharField(default = '', max_length = 23)
-
