33bc5a02a3e7955d34a4184a2cf6fbfc1b4d8a4d
Visionaryofthefuture <somud2004@gmail.com>
2024-01-31 12:11:35 +0530

    made static file

--- Hireme/urls.py
+++ Hireme/urls.py
@@ -1,8 +1,12 @@
 from django.http import HttpResponse
 from django.urls import path
+from django.conf.urls.static import static
+from django.conf import settings
 from . import views
 
 urlpatterns = [
     path('', views.homepage),
     path('login/', views.login, name = "login")
 ]
+if(settings.DEBUG):
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS[0])
